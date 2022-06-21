d627b7c37a5fe65939ed42f5129640c064a96a83
张大鹏 <pygosuperman@outlook.com>
2022-06-21 17:55:38 +0800

    新增：密码生成器、密码文件生成和更新

---
+++ examples/aes_dump_load_update/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zhangdapeng520/zdpgo_log"
+	"github.com/zhangdapeng520/zdpgo_password"
+	"github.com/zhangdapeng520/zdpgo_password/generator"
+)
+
+func main() {
+	p := zdpgo_password.New(zdpgo_log.Tmp)
+	data := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{"zhangdapeng520", "zhangdapeng520"}
+
+	// 将对象加密保存为文件
+	err := p.AesDump("email", &data)
+	if err != nil {
+		panic(err)
+	}
+
+	// 读取文件，解析为JSON对象
+	err = p.AesLoad("email", &data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("读取数据成功", data)
+
+	// 更新文件
+	key := generator.DefaultGenerator.GenerateByLength(32)
+	err = p.AesUpdate("email", &data, key)
+	if err != nil {
+		panic(err)
+	}
+
+	// 重新加载
+	err = p.AesLoad("email", &data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("读取更新数据成功", data)
+}
